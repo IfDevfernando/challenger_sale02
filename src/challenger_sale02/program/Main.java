9153ad56d3ec3085b8be5c63388a2dc2131e7b77
IfDevfernando <ifsoftfernando@gmail.com>
2024-04-15 20:30:16 -0300

    correct name challenger

--- src/challenger_sale02/program/Main.java
+++ src/challenger_sale02/program/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-System.out.println("============= CHALLENGER SALE 01 ==============");
+System.out.println("============= CHALLENGER SALE 02 ==============");
 		
 		String path ="C:\\Users\\usedo\\OneDrive\\Documentos\\curso_devSuperiro_spring\\Java_Devsuperiror_expert\\in.csv";
 		
